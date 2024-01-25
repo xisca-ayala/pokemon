b5d3d0a03d83ecd564cc2cc1e11a6260eef92195
xisca-ayala <xisca.ayala@iMac-de-Antoni.bbrouter>
2024-01-25 11:28:47 +0100

    card

--- app.js
+++ app.js
@@ -11,47 +11,70 @@ apiButton.addEventListener('click', getPokemon);
 
 function getPokemon(){
     const searchInput = document.getElementById('searchInput').value; 
-    const pokeName = document.getElementById('pokeName');
-    const pokePhoto = document.getElementById('pokePhoto');
-    const pokeAbilities = document.getElementById('pokeAbilities')
-    const pokeId = document.getElementById('pokeId');
-
     let url= "https://pokeapi.co/api/v2/pokemon/" + searchInput;
-
-    let param={
+    const card = document.getElementById('card'); 
+    let html = '';
+       let param={
             headers: {"Content-type": "application/json; charset = UTF-8"},
             method: "GET"
         }
+    if(searchInput !== 0){
+        fetch(url, param)
+            .then(res=>{
+                return res.json();
+            })
+            .then(data=>{
+                console.log(data)
+                if(!data.error){
+                    let pokeName = ''; 
+                    pokeName.innerText = data.name;  
+                    let pokeId = Number; 
+                    pokeId.innerText = data.id; 
+                    let pokePhoto = ''; 
+                    pokePhoto.src = data.sprites.front_default;
 
-    fetch(url, param)
-        .then(res=>{
-            return res.json();
-        })
-        .then(data=> {
-            console.log(data)
-            if(!data.error){
-                pokeName.value = data.name; 
-                console.log(pokeName.value); 
-                pokeName.innerText = data.name; 
-                pokeId.value = data.id;
-                console.log(pokeId.value); 
-                pokeId.innerText = data.id; 
-                pokePhoto.value = data.sprites.front_default;
-                console.log(pokePhoto.value);
-                pokePhoto.src = data.sprites.front_default;
-                // pokeAbilities.value = data.abilities[0].ability.name; 
-                // console.log(pokeAbilities.value);
-                // map per accedir a diferents habilitats
-              
-            }else{
-                showToast("ERROR: " + data.mensaje, "bg-danger")
-            }
-        })
-        .catch(error=>{
-            console.error(error);
-        })
-    }
+                    // let pokeAbilities= []; 
+                    // pokeAbilities.innerText = data.abilities[0].ability.name; 
 
-  
+                    html = ' <div class="cardBox">'+
+                    '<div class="card" style="width: 20rem;">'+
+                        '<img src="'+ data.sprites.front_default + '" class="card-img-top" alt="..." id="pokePhoto">'+
+                        '<div class="card-body">'+
+                            '<h5 class="card-title">'+ data.name +'</h5>'+
+                            '<p>'+ data.id + '</p>'+
+                    ' </div>'+
+                        '<table class="table">'+
+                        '<thead>'+
+                            '<th scope="row"></th>' +
+                                '<td colspan="2" class="table-active">Habilidad</td>'+
+                            '</thead>'+
+                            '<tbody>'+
+                            '<tr>' +
+                            '<th scope="row">1</th>' +
+                            '<td colspan="2" class="table-active">Larry the Bird</td>'+
+                            '</tr>'+
+                            ' <tr>'+
+                            ' <th scope="row">2</th>'+
+                                '<td colspan="2" class="table-active">Larry the Bird</td>'+
+                            '</tr>'+
+                            ' <tr>'+
+                            '<th scope="row">3</th>' +
+                            '<td colspan="2" class="table-active">Larry the Bird</td>' +
+                            '</tr>' +
+                            '</tbody>'+
+                            '</table>'+
+                        '</div>' +
+                     '</div>;'
+
+                card.innerHTML = html;   
+                }else{
+                    showToast("ERROR: " + data.mensaje, "bg-danger")
+                }
+            })
+            .catch(error=>{
+                console.error(error);
+            })
+        }
+}
 
     
