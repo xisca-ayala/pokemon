9106a410d4f4d500eeef6d7404c34ee36f81ef24
xisca-ayala <xisca.ayala@iMac-de-Antoni.bbrouter>
2024-01-27 20:50:30 +0100

    keydown i abilities

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
-class pokemon{
-    constructor(name, photo, abilities){
+class Pokemon{
+    constructor(id, name, photo, abilities, types){
+        this.id = id;
         this.name = name;
         this.photo = photo;
         this.abilities = abilities; 
+        this.types = types;
     }
 }
 
 const apiButton = document.getElementById('apiButton');
 apiButton.addEventListener('click', getPokemon);
 
+const inputId = document.getElementById('searchInput');
+inputId.addEventListener('keydown', function onEvent(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        getPokemon();
+    }
+});
+
 function getPokemon(){
     const searchInput = document.getElementById('searchInput').value; 
     let url= "https://pokeapi.co/api/v2/pokemon/" + searchInput;
     const card = document.getElementById('card'); 
     let html = '';
-       let param={
-            headers: {"Content-type": "application/json; charset = UTF-8"},
-            method: "GET"
-        }
-    if(searchInput !== 0){
+    let param = {
+        headers: {"Content-type": "application/json; charset = UTF-8"},
+        method: "GET"
+    }
+    if(searchInput !== 0) {
         fetch(url, param)
             .then(res=>{
                 return res.json();
@@ -26,46 +36,29 @@ function getPokemon(){
             .then(data=>{
                 console.log(data)
                 if(!data.error){
-                    let pokeName = ''; 
-                    pokeName.innerText = data.name;  
-                    let pokeId = Number; 
-                    pokeId.innerText = data.id; 
-                    let pokePhoto = ''; 
-                    pokePhoto.src = data.sprites.front_default;
-
-                    // let pokeAbilities= []; 
-                    // pokeAbilities.innerText = data.abilities[0].ability.name; 
-
-                    html = ' <div class="cardBox">'+
-                    '<div class="card" style="width: 20rem;">'+
-                        '<img src="'+ data.sprites.front_default + '" class="card-img-top" alt="..." id="pokePhoto">'+
-                        '<div class="card-body">'+
-                            '<h5 class="card-title">'+ data.name +'</h5>'+
-                            '<p>'+ data.id + '</p>'+
-                    ' </div>'+
-                        '<table class="table">'+
-                        '<thead>'+
-                            '<th scope="row"></th>' +
-                                '<td colspan="2" class="table-active">Habilidad</td>'+
-                            '</thead>'+
-                            '<tbody>'+
-                            '<tr>' +
-                            '<th scope="row">1</th>' +
-                            '<td colspan="2" class="table-active">Larry the Bird</td>'+
-                            '</tr>'+
-                            ' <tr>'+
-                            ' <th scope="row">2</th>'+
-                                '<td colspan="2" class="table-active">Larry the Bird</td>'+
-                            '</tr>'+
-                            ' <tr>'+
-                            '<th scope="row">3</th>' +
-                            '<td colspan="2" class="table-active">Larry the Bird</td>' +
-                            '</tr>' +
-                            '</tbody>'+
-                            '</table>'+
-                        '</div>' +
-                     '</div>;'
-
+                    const pokemon = setPokemon(data);
+                    html = '<div class="cardBox">'+
+                                '<div class="card" style="width: 20rem;">'+
+                                    '<img src="'+ pokemon.photo + '" class="card-img-top" alt="..." id="pokePhoto">'+
+                                    '<div class="card-body">'+
+                                        '<h5 class="card-title">#' + pokemon.id + ' - ' + capitalize(pokemon.name) +'</h5>'+
+                                    '</div>'+
+                                    '<table class="table">'+
+                                        '<thead>'+
+                                            '<th scope="row"></th>' +
+                                            '<td colspan="2" class="table-active">Habilidades</td>'+
+                                        '</thead>'+
+                                        '<tbody>';
+                                        pokemon.abilities.forEach(function (ability, index) {
+                                            html += '<tr>' +
+                                                        '<th scope="row">' + (index+1) + '</th>' +
+                                                        '<td colspan="2" class="table-active">' + capitalize(ability.ability.name) + '</td>'+
+                                                    '</tr>';
+                                        });
+                                html += '</tbody>'+
+                                    '</table>'+
+                                '</div>' +
+                            '</div>';
                 card.innerHTML = html;   
                 }else{
                     showToast("ERROR: " + data.mensaje, "bg-danger")
@@ -74,7 +67,21 @@ function getPokemon(){
             .catch(error=>{
                 console.error(error);
             })
-        }
+    }
+}
+
+function setPokemon(data) {
+    let pokemon = new Pokemon(
+        data.id,
+        data.name,
+        data.sprites.front_default, 
+        data.abilities,
+        data.types);
+    return pokemon;
+}
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
     
